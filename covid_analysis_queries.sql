d9bd3042925174770519e4e24976867634275213
mattbr92 <mattbr92@gmail.com>
2026-03-21 20:33:05 -0500

    Create covid_analysis_queries.sql
    This repository contains a series of SQL queries designed to analyze the global and regional impact of the COVID-19 pandemic. Using the **Google Cloud BigQuery Public Dataset**, I performed data cleaning, mortality rate calculations, and trend identification.

---
+++ covid_analysis_queries.sql
@@ -0,0 +1,56 @@
+SELECT
+  country_region,
+  ROUND(SUM(deaths) / 1000000,2) AS Total_deaths
+  --## numbers in millions.
+FROM
+  bigquery-public-data.covid19_open_data.compatibility_view
+GROUP BY
+  country_region
+ORDER BY
+  Total_deaths DESC
+LIMIT 10;
+---------------------------------------------------------------------------
+SELECT
+  province_state,
+  ROUND(SUM(confirmed) / 1000000,2) AS Total_confirmed,
+  ROUND(SUM(deaths) / 1000000,2) AS Total_deaths,
+  ROUND((SUM(deaths) / SUM(confirmed)) * 100,2) AS Mortality_rate
+  --## numbers in millions.
+FROM
+  bigquery-public-data.covid19_open_data.compatibility_view
+WHERE
+  country_region = 'Colombia'
+GROUP BY
+  province_state
+ORDER BY
+  province_state ASC;
+
+SELECT
+  country_region,
+  ROUND(MAX(confirmed)/1000000,2) AS peak_cases --## numbers in millions
+FROM bigquery-public-data.covid19_open_data.compatibility_view
+WHERE
+  country_region IN ('Colombia', 'Brazil', 'Argentina', 'Mexico', 'Peru')
+GROUP BY
+  country_region
+  ORDER BY peak_cases DESC;
+----------------------------------------------------------------------------
+WITH mexico_summary AS
+  (
+      SELECT
+        province_state,
+        ROUND(SUM(confirmed)/1000000,2) AS total_confirmed,
+        ROUND(SUM(deaths)/1000000,2) AS total_deaths
+      FROM bigquery-public-data.covid19_open_data.compatibility_view
+      WHERE
+        country_region = 'Mexico'
+      GROUP BY
+        province_state
+  )      
+SELECT
+  *
+FROM mexico_summary
+WHERE
+  mexico_summary.total_deaths > 0.05
+ORDER BY 
+  mexico_summary.total_deaths DESC
